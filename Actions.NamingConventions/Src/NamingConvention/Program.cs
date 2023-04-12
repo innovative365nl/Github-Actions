a1c36072e209e649dfa26f1b512ec88e37c13773
Vincent Boots <vincent.boots@innovative.nl>
2023-04-12 10:50:07 +0200

    updatte naming to new format

--- Actions.NamingConventions/Src/NamingConvention/Program.cs
+++ Actions.NamingConventions/Src/NamingConvention/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text;
 using NamingConvention;
 
 if (args.Length <= 0) throw new ArgumentException("Please supply arguments.");
@@ -7,8 +8,6 @@ if (args.Length <= 0) throw new ArgumentException("Please supply arguments.");
 
 const string path             = "unknown";
 const string argumentErrorStr = "Please supply arguemnt: `{0}` to action.";
-
-
 
 var folderName = args[1];
 if (string.IsNullOrEmpty(folderName) || folderName.Equals(path))
@@ -27,12 +26,26 @@ product = product.Remove(8);
 var logic = folderName.Split(".")[1];
 var type  = InputHelper.ReturnType(folderName: ref folderName, argumentErrorStr: argumentErrorStr);
 
+var rgName = $"{environment}-{product}-Rg-Weu";
+var apimName = $"{environment}-{product}-Main-Apim-Weu";
+var apiId = $"{type.Remove(4)}-{logic}";
+var apiPath = $"{type.ToLower()}/{logic.ToLower()}";
+var functionName = $"{environment}-{product}-{type.Remove(4)}-{logic}-Func-001-Weu";
 
-Console.WriteLine($"::set-output name=RgName::{environment}-{product}-Rg-Weu");
-Console.WriteLine($"::set-output name=ApimName::{environment}-{product}-Main-Apim-Weu");
-Console.WriteLine($"::set-output name=ApiId::{type.Remove(4)}-{logic}");
-Console.WriteLine($"::set-output name=ApiPath::{type.ToLower()}/{logic.ToLower()}");
-Console.WriteLine($"::set-output name=FunctionName::{environment}-{product}-{type.Remove(4)}-{logic}-Func-001-Weu");
 
+var gitHubOutputFile = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
+if (!string.IsNullOrWhiteSpace(gitHubOutputFile))
+{
+    await using StreamWriter textWriter = new(gitHubOutputFile, true, Encoding.UTF8);
+    textWriter.WriteLine($"RgName={rgName}");
+    textWriter.WriteLine($"ApimName={apimName}");
+    textWriter.WriteLine($"ApiId={apiId}");
+    textWriter.WriteLine($"ApiPath={apiPath}");
+    textWriter.WriteLine($"FunctionName={functionName}");
+}
+
+await ValueTask.CompletedTask;
+
+Environment.Exit(0);
 
 
