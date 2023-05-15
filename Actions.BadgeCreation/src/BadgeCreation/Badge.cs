881ae4b69f0990e4a68591c6828c454210807191
Matthijs van der Plas <mvdplas84@ziggo.nl>
2023-05-15 11:03:40 +0200

    Initial Commijt

---
+++ Actions.BadgeCreation/src/BadgeCreation/Badge.cs
@@ -0,0 +1,29 @@
+﻿namespace BadgeCreation;
+
+public class Badge
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public StatusEnum Status { get; set; }
+
+    public static Badge Create(string name, StatusEnum status)
+    {
+        var badge = new Badge()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = name,
+            Description = "This is a badge",
+            Status = status
+        };
+        
+        return badge;
+    } 
+}
+
+public enum StatusEnum
+{
+    Success,
+    Failure,
+    Pending
+}
