0c91dd26d30ff037e39651cce67ca3f995bfbb38
Vincent Boots <9152628+vdboots@users.noreply.github.com>
2023-12-14 14:48:12 +0100

    Update Program.cs
    Signed-off-by: Vincent Boots <9152628+vdboots@users.noreply.github.com>

--- Actions.NamingConventions/Src/NamingConvention/Program.cs
+++ Actions.NamingConventions/Src/NamingConvention/Program.cs
@@ -33,10 +33,13 @@ var apiId = $"{type.Remove(4)}-{logic}";
 var apiPath = $"{type.ToLower()}/{logic.ToLower()}";
 
 //temporary change
-if (type != "Portal" && type != "Connectors")
-    type = type.Remove(4);
-if (type != "Connectors")
-    type = "Connector"
+if (type == "Portal" )
+{
+}
+else if (type == "Connectors")
+    type = "Connector";
+else 
+    type = type.remove(4);
 var functionName = $"{environment}-{product}-{type}-{logic}-Func-01-Weu";
 var webAppName = $"{environment}-{product}-{type}-{logic}-webApp-Weu";
 
