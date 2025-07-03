18b1f97d25d19eaab775396a5ced0b9996fc6d64
Vincent Boots <9152628+vdboots@users.noreply.github.com>
2025-07-03 11:05:52 +0200

    Update InputHelper.cs
    Signed-off-by: Vincent Boots <9152628+vdboots@users.noreply.github.com>

--- Actions.NamingConventions/Src/NamingConvention/InputHelper.cs
+++ Actions.NamingConventions/Src/NamingConvention/InputHelper.cs
@@ -4,7 +4,13 @@ public static class InputHelper
 {
     public static string ReturnType(ref string folderName, string argumentErrorStr)
     {
-        var type = folderName.Split(".")[0];
+        string type;
+        if(folderName.Split(".")[0].ToLower() == "automate")
+        {
+            type = folderName.Split(".")[1]
+        }
+        else
+             type = folderName.Split(".")[0];
         if (type.Equals("unknown"))
             throw new ArgumentException(string.Format(argumentErrorStr, type));
         else if (type.ToLower().Equals("orchestrators"))
